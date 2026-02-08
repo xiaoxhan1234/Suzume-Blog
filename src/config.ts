31709c6031c1afb7eb4cf7a38790672fe6145fa9
xiaoxhan1234 <3084058513@qq.com>
2026-02-08 15:13:14 +0800

    Update 1 file: config.ts

--- src/config.ts
+++ src/config.ts
@@ -295,7 +295,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 					name: "统计",
-					url: "https://cloud.umami.is/share/Da4GLnhCTeTBEaVR",
+					url: "https://umami.zako.wf/share/kDBuoFVIskljMXJp",
 					external: true,
 					icon: "fa6-brands:jsfiddle",
 			},
